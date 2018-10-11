1d831ecebd6bada58dd669543e0c4b1f1f6c1625
Arnau Gallofré <masokismguys@gmail.com>
2018-10-11 09:24:26 +0200

    module coll player & debug created

---
+++ Color-Jump/Motor2D/j1Player.h
@@ -0,0 +1,47 @@
+#ifndef __ModulePlayer_H__
+#define __ModulePlayer_H__
+
+#include "j1Module.h"
+#include "Animation.h"
+#include "Globals.h"
+#include "p2Point.h"
+
+struct SDL_Texture;
+struct Collider;
+
+class ModulePlayer : public j1Module
+{
+public:
+	ModulePlayer();
+	~ModulePlayer();
+
+	bool Start();
+	update_status Update();
+	bool CleanUp();
+	void OnCollision(Collider* collider1, Collider* collider2);
+	void resetPlayer();
+
+public:
+	bool enable_movement = false;
+	int counter = 0;
+	int speed = 2;
+	SDL_Texture * graphics = nullptr;
+	Animation idle;
+	Animation forward;
+	Animation backward;
+	Animation upward;
+	Animation downward;
+	Animation upwardreturn;
+	Animation downwardreturn;
+	Animation* current_animation;
+	iPoint position;
+	iPoint camera_offset;
+	Uint32 start_time;
+	Uint32 aux_time;
+	Collider* collider;
+	bool dead = false;
+	bool godmode = false;
+	char _godmode[8] = "godmode";
+};
+
+#endif
